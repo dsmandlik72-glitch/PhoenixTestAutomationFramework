35b4540ea9199a84d8e0e93e8349435afbc2a7e9
Dhananjay S Mandlik <dsmandlik72@gmail.com>
2026-03-09 11:53:16 +0530

    Requirement 7 - Create Job API Test
    Created POJO Classes for the Create Job API Test.
    
    And also Introduced Basic Tests using Spec Util.

--- src/test/java/com/api/utils/SpecUtil.java
+++ src/test/java/com/api/utils/SpecUtil.java
@@ -55,6 +55,24 @@ public class SpecUtil {
 
 		return requestSpecification;
 	}
+	
+	public static RequestSpecification requestSpecWithAuth(Role role, Object payload) {
+		// To take care of common request sections(Methods)
+
+		RequestSpecification requestSpecification = new RequestSpecBuilder()
+				.setBaseUri(ConfigManager.getProperty("BASE_URI"))
+				.setContentType(ContentType.JSON)
+				.setAccept(ContentType.JSON)
+				.addHeader("Authorization", AuthTokenProvider.getToken(role))
+				.setBody(payload)
+				.log(LogDetail.URI)
+				.log(LogDetail.METHOD)
+				.log(LogDetail.HEADERS)
+				.log(LogDetail.BODY)
+				.build();
+
+		return requestSpecification;
+	}
 
 	// Post, Put, Patch
 

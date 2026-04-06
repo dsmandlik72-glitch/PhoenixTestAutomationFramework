2dee4666bdbd6fc448dcd88d0a3434017d8d0e20
Dhananjay S Mandlik <dsmandlik72@gmail.com>
2026-04-06 22:52:23 +0530

    Refactored the Project Structure.
    CSVReaderUtil: Load the CSV File and Map it to UserBean Class.
    
    UserBean: Bean Class for CSV mapping and will be used by DataProviders.

--- src/test/java/com/api/utils/CSVReaderUtil.java
+++ src/test/java/com/api/utils/CSVReaderUtil.java
@@ -1,23 +1,32 @@
-package com.demo.csv;
+package com.api.utils;
 
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.List;
 
+import com.dataproviders.api.bean.UserBean;
 import com.opencsv.CSVReader;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
-import com.opencsv.exceptions.CsvException;
 
-public class ReadCSVFile_MapToPOJO {
-	public static void main(String[] args) throws IOException, CsvException {
+public class CSVReaderUtil {
+/*
+ * Constructor is private
+ * 
+ * static - static methods!
+ * Job: Help me Read the CSV file and Map it a Bean
+ */
+private CSVReaderUtil() {
+	//Due to Private Constructor No one can create Object of CSVReaderUtil class outside the Class.
+	//Singleton class alos has Private Constructors.
+}
+	
+public static void loadCSV(String pathOfCSVFile)  {
 		
-		InputStream is = Thread.currentThread().getContextClassLoader().getResourceAsStream("testData/LoginCreds.csv");
+		InputStream is = Thread.currentThread().getContextClassLoader().getResourceAsStream(pathOfCSVFile);
 		InputStreamReader isr = new InputStreamReader(is);
 		CSVReader csvReader = new CSVReader(isr);// CSVReader Constructor
 		
-//Write the code to Map the CSV to POJO
 		
 		CsvToBean<UserBean> csvToBean=new CsvToBeanBuilder(csvReader)
 				.withType(UserBean.class)
@@ -30,3 +39,4 @@ public class ReadCSVFile_MapToPOJO {
 
 	}
 }
+

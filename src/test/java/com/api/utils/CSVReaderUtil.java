5aee3269e20a4a5143ad39bedcda96079f122edd
Dhananjay S Mandlik <dsmandlik72@gmail.com>
2026-04-07 23:51:25 +0530

    Introduced LoginAPI Data Driven test for POC of open CSV Libray!
    Introduced the dataprovider util to pass the test data to LoginApiTest
    and the tests will be executed parallel!

--- src/test/java/com/api/utils/CSVReaderUtil.java
+++ src/test/java/com/api/utils/CSVReaderUtil.java
@@ -2,6 +2,7 @@ package com.api.utils;
 
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.Iterator;
 import java.util.List;
 
 import com.dataproviders.api.bean.UserBean;
@@ -10,33 +11,27 @@ import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
 
 public class CSVReaderUtil {
-/*
- * Constructor is private
- * 
- * static - static methods!
- * Job: Help me Read the CSV file and Map it a Bean
- */
-private CSVReaderUtil() {
-	//Due to Private Constructor No one can create Object of CSVReaderUtil class outside the Class.
-	//Singleton class alos has Private Constructors
-}
-	
-public static void loadCSV(String pathOfCSVFile)  {
-		
+	/*
+	 * Constructor is private
+	 * 
+	 * static - static methods! Job: Help me Read the CSV file and Map it a Bean
+	 */
+	private CSVReaderUtil() {
+		// Due to Private Constructor No one can create Object of CSVReaderUtil class
+		// outside the Class.
+		// Singleton class alos has Private Constructors
+	}
+
+	public static Iterator<UserBean> loadCSV(String pathOfCSVFile) {
+
 		InputStream is = Thread.currentThread().getContextClassLoader().getResourceAsStream(pathOfCSVFile);
 		InputStreamReader isr = new InputStreamReader(is);
 		CSVReader csvReader = new CSVReader(isr);// CSVReader Constructor
-		
-		
-		CsvToBean<UserBean> csvToBean=new CsvToBeanBuilder(csvReader)
-				.withType(UserBean.class)
-				.withIgnoreEmptyLine(true)
-				.build();
-		
-		List<UserBean> userList=csvToBean.parse();
-		System.out.println(userList);
-	
 
+		CsvToBean<UserBean> csvToBean = new CsvToBeanBuilder(csvReader).withType(UserBean.class)
+				.withIgnoreEmptyLine(true).build();
+
+		List<UserBean> userList = csvToBean.parse();
+		return userList.iterator();
 	}
 }
-

3134a502187970918ca68c71b8e66341d6230fcf
Dhananjay <dsmandlik72@gmail.com>
2026-07-28 19:37:11 +0530

    Created CustomerDao and CustomerDBModel for requirement 13.

---
+++ src/test/java/com/database/model/DemoRunner.java
@@ -0,0 +1,12 @@
+package com.database.model;
+
+public class DemoRunner {
+
+	public static void main(String[] args) {
+
+		
+		CustomerDBModel customer=new CustomerDBModel("Dhananjay", "Mandlik", "9405167371", "9096123433", "dsmandlik72@gmail.com", "dmandlik92@gmail.com");
+		System.out.println(customer);
+	}
+
+}

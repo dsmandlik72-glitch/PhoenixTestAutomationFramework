a83f68eb768fc90b5001f9e998290e60abdf793e
Dhananjay <dsmandlik72@gmail.com>
2026-08-09 15:58:44 +0530

    Did the changes in the testng.xl and testng-datadriven.xml file as there was issue in the classes which

--- src/test/java/com/api/tests/CreateJobAPIWithDBValidationTest.java
+++ src/test/java/com/api/tests/CreateJobAPIWithDBValidationTest.java
@@ -58,8 +58,8 @@ public class CreateJobAPIWithDBValidationTest {
 				"India", "Maharashtra");
 		System.out.println(customerAddress.apartment_name());
 
-		customerProduct = new CustomerProduct(getTimeWithDaysAgo(10), "56297090442606", "56297090442606",
-				"56297090442606", getTimeWithDaysAgo(10), Product.NEXUS_2.getCode(), Model.NEXUS_2_BLUE.getCode());
+		customerProduct = new CustomerProduct(getTimeWithDaysAgo(10), "46987090442606", "46987090442606",
+				"46987090442606", getTimeWithDaysAgo(10), Product.NEXUS_2.getCode(), Model.NEXUS_2_BLUE.getCode());
 
 		Problems problems = new Problems(Problem.SMARTPHONE_IS_RUNNING_SLOW.getCode(), "Battery Issue");
 

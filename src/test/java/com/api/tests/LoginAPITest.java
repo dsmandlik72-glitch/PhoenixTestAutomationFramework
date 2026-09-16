833483794f153b3f6ae53f3eab989c3894dfbd53
Dhananjay <dsmandlik72@gmail.com>
2026-09-16 20:41:33 +0530

    Created the Auth Service class to maintain the login api at one place.

--- src/test/java/com/api/tests/LoginAPITest.java
+++ src/test/java/com/api/tests/LoginAPITest.java
@@ -10,6 +10,8 @@ import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import com.api.request.model.UserCredentials;
+import com.api.services.AuthService;
+
 import static com.api.utils.SpecUtil.*;
 
 import static io.restassured.module.jsv.JsonSchemaValidator.*;
@@ -17,10 +19,12 @@ import static io.restassured.module.jsv.JsonSchemaValidator.*;
 public class LoginAPITest {
 
 	UserCredentials userCredentials;
+	private AuthService authService;
 
-	@BeforeMethod(description="Create the payload for login api")
+	@BeforeMethod(description = "Create the payload for login api")
 	public void setUp() {
 		userCredentials = new UserCredentials("iamfd", "password");
+		authService = new AuthService();
 
 	}
 
@@ -29,15 +33,11 @@ public class LoginAPITest {
 
 	{
 		// Rest Assured Code
-
-		given()
-		.spec(requestSpec(userCredentials))
-		.when()
-		.post("login")
+		authService.login(userCredentials)
 		.then()
 		.spec(responseSpec_OK())
-				.and()
-				.body("message", equalTo("Success"))
+		.and()
+		.body("message", equalTo("Success"))
 				.and()
 				.body(matchesJsonSchemaInClasspath("response-schema/LoginResponseSchema.json"));
 
